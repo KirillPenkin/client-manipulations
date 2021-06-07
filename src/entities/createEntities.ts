df559efb580888894b9adb8cc06d0a050e8d3c9f
Kirill Penkin <kirill.penkin@arcadia.spb.ru>
2021-06-07 16:03:00 +0300

    create entities

---
+++ src/entities/createEntities.ts
@@ -0,0 +1,29 @@
+import { getRandomPart, getRandomResource } from "./const";
+
+const entitiesNumber = 1000000;
+
+export interface ICandidate {
+    firstName: string;
+    lastName: string;
+    permitted: Set<string>;
+}
+
+export const entities = [];
+
+const createEntity = (): ICandidate => {
+    const permitted = new Set<string>();
+    permitted.add(getRandomResource());
+    permitted.add(getRandomResource());
+    permitted.add(getRandomResource());
+    permitted.add(getRandomResource());
+    permitted.add(getRandomResource());
+    return {
+        firstName: `${getRandomPart()} ${getRandomPart()}`,
+        lastName: `${getRandomPart()} ${getRandomPart()}`,
+        permitted,
+    }
+}
+
+for (let i=0; i< entitiesNumber; i++) {
+    entities.push(createEntity);
+}
