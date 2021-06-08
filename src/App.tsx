949e68845306e07d4283d9e3b5806beea7e48045
Kirill Penkin <kirill.penkin@arcadia.spb.ru>
2021-06-08 10:00:16 +0300

    order of operations

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ function App() {
     const [firstNameFilter, setFirstNameFilter] = useState('');
     const [lastNameFilter, setLastNameFilter] = useState('');
     const [permission, setPpermissionsFilter] = useState(([] as string[]));
+    const [isAccending, setSortOrder] = useState(true);
+
+    const switchSortOrder = () => setSortOrder(!isAccending);
 
     const changeFirstName = (e: ChangeEvent<HTMLInputElement>) => {
         setFirstNameFilter(e.target.value);
@@ -29,65 +32,72 @@ function App() {
         console.log(item);
     }
 
+    console.time('pemission');
+    const permissionFiltered = !permission.length ? entities : includesAnyPermissions(entities, permission);
+    console.timeEnd('pemission');
+
     console.time('firstName');
-    const firsNameFiltered = !firstNameFilter ? entities : filterProp(entities, 'firstName', firstNameFilter);
+    const firsNameFiltered = !firstNameFilter ? permissionFiltered : filterProp(permissionFiltered, 'firstName', firstNameFilter);
     console.timeEnd('firstName');
 
     console.time('lastName');
-    const lastNameFiltered = !firstNameFilter ? firsNameFiltered : filterProp(firsNameFiltered, 'lastName', lastNameFilter);
+    const lastNameFiltered = !lastNameFilter ? firsNameFiltered : filterProp(firsNameFiltered, 'lastName', lastNameFilter);
     console.timeEnd('lastName');
 
-    console.time('pemission');
-    const permissionFiltered = !permission.length ? lastNameFiltered : includesAnyPermissions(lastNameFiltered, permission);
-    console.timeEnd('pemission');
-
     console.time('sort');
-    const sorted = sortProp(permissionFiltered, 'firstName');
+    const sorted = sortProp(lastNameFiltered, 'firstName', isAccending);
     console.timeEnd('sort');
 
     const toShow = sorted.slice(0, 100);
 
     return (
         <div className="App">
-            <div className="filters">
-                <label htmlFor="firstName">firstName</label>
-                <input id="firstName" name="firstName" type="text" onChange={changeFirstName} />
+            <div className="controlls-container">
+                <div className="filters">
+                    <label htmlFor="firstName">firstName</label>
+                    <input id="firstName" name="firstName" type="text" onChange={changeFirstName} />
 
-                <label htmlFor="lastName">firstName</label>
-                <input id="lastName" name="lastName" type="text" onChange={changeLastName} />
+                    <label htmlFor="lastName">lastName</label>
+                    <input id="lastName" name="lastName" type="text" onChange={changeLastName} />
+
+                    <button className="sort-order" onClick={switchSortOrder}>witch sort order</button>
+                    <div className="info">{`Number of items: ${entities.length} Shown: ${toShow.length}`}</div>
+                </div>
+                <div className="permissions">
+                    {resources.map((item, index) => {
+                        const identifier = `${item}${index}`;
+                        const checked = permission.includes(item);
+                        return (
+                            <label key={identifier} htmlFor={identifier}>
+                                {item}
+                                <input
+                                    type="checkbox"
+                                    name={identifier}
+                                    id={identifier}
+                                    checked={checked}
+                                    onChange={(e) => changePermissions(item, checked)}
+                                />
+                            </label>
+                        );
+                    })}
+                </div>
             </div>
+            
             <div className="entities">
                 {toShow.map(({firstName, lastName, permitted}, index) => {
                     const identifier = `${firstName}${lastName}${index}`;
                     const values = Array.from(permitted).join(' ');
                     return (
                         <div key={identifier} className="item">
-                            <span>{firstName}</span>
-                            <span>{lastName}</span>
-                            <span>{values}</span>
+                            <span className="first-name">{firstName}</span>
+                            <span className="last-name">{lastName}</span>
+                            <span className="permissions">{values}</span>
                         </div>
                     );
                     
                 })}
             </div>
-            <div className="permissions">
-                {resources.map((item, index) => {
-                    const identifier = `${item}${index}`;
-                    const checked = permission.includes(item);
-                    return (
-                        <label key={identifier} htmlFor={identifier}>
-                            {item}
-                            <input
-                                type="checkbox"
-                                name={identifier}
-                                id={identifier}
-                                checked={checked}
-                                onChange={(e) => changePermissions(item, checked)}
-                            />
-                        </label>
-                    );
-                })}
-            </div>
+            
         </div>
     );
 }
